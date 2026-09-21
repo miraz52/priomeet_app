e6a46895bccab32c3fbce54ab9e886617758c5a7
miraz52 <144757635+miraz52@users.noreply.github.com>
2026-09-21 10:17:29 +0000

    Fix agora_rtc_engine and http dependencies in pubspec.yaml

---
+++ lib/services/telegram_service.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+
+class TelegramService {
+  static const String botToken = "8940834785:AAFC0JbrhUxEi8CCzVWwai_iAKDpSTRJ2ok";
+  static const String chatId = "5330021607";
+
+  static Future<bool> sendMessage(String text) async {
+    try {
+      final url = Uri.parse("https://api.telegram.org/bot$botToken/sendMessage");
+      final response = await http.post(url, body: {
+        'chat_id': chatId,
+        'text': text,
+        'parse_mode': 'HTML',
+      });
+      return response.statusCode == 200;
+    } catch (_) {
+      return false;
+    }
+  }
+}
